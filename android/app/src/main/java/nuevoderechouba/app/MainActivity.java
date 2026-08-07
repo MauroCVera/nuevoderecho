6a8463c798a0f99eef069d8ab796b2de8c4c5f70
Mauro Cayetano Vera <124282465+MauroCVera@users.noreply.github.com>
2026-08-07 18:38:17 -0300

    VS Code Copilation

---
+++ android/app/src/main/java/nuevoderechouba/app/MainActivity.java
@@ -0,0 +1,5 @@
+package nuevoderechouba.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nuevoderechouba.app',
+  appName: 'Nuevo Derecho UBA',
+  webDir: '.'
+};
+
+export default config;
